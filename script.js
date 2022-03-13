b6a4d7dba7bb861d76b34c7cbdd7ee684b6777e8
Walker <walker.m.oliver@gmail.com>
2022-03-13 15:29:07 -0400

    Add functionality to background buttons

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 "use strict";
+
+const grid = document.querySelector(".grid");
+const blackBackgroundBtn = document.querySelector("#black-bkg");
+const whiteBackgroundBtn = document.querySelector("#white-bkg");
+const greyBackgroundBtn = document.querySelector("#grey-bkg");
+
+//Buttons to change background color
+blackBackgroundBtn.addEventListener("click", () => {
+  grid.style.backgroundColor = "black";
+});
+whiteBackgroundBtn.addEventListener("click", () => {
+  grid.style.backgroundColor = "white";
+});
+greyBackgroundBtn.addEventListener("click", () => {
+  grid.style.backgroundColor = "rgb(150, 148, 148)";
+});
+
+//Buttons to change grid size
+
+//Buttons to change sketch color
+
+//Erase button
