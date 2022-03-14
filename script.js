b1c1280dbab4bca24cb7045dc0b815e73a80ef1f
Walker <walker.m.oliver@gmail.com>
2022-03-14 17:29:05 -0400

    Write function to create 16x16 div grid

--- script.js
+++ script.js
@@ -5,7 +5,29 @@ const blackBackgroundBtn = document.querySelector("#black-bkg");
 const whiteBackgroundBtn = document.querySelector("#white-bkg");
 const greyBackgroundBtn = document.querySelector("#grey-bkg");
 
-//Buttons to change background color
+//Change grid size
+
+//function to create grid of divs
+function addDiv() {
+  for (let i = 0; i < 4; i++) {
+    const newColumn = document.createElement("div");
+    newColumn.classList.add("grid-columns");
+
+    for (let y = 0; y < 4; y++) {
+      const newSquare = document.createElement("div");
+      newSquare.classList.add("grid-squares");
+      newColumn.appendChild(newSquare);
+    }
+    grid.appendChild(newColumn);
+  }
+}
+
+addDiv();
+
+//create 16 divs
+//arrange the divs in a grid
+
+//Change background color
 blackBackgroundBtn.addEventListener("click", () => {
   grid.style.backgroundColor = "black";
 });
@@ -16,8 +38,6 @@ greyBackgroundBtn.addEventListener("click", () => {
   grid.style.backgroundColor = "rgb(150, 148, 148)";
 });
 
-//Buttons to change grid size
-
-//Buttons to change sketch color
+//Change sketch color
 
 //Erase button
