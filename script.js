9413fec50c4e00dbac0ce195c5ff479940b7868b
Walker <walker.m.oliver@gmail.com>
2022-03-16 10:49:48 -0400

    Make sketch buttons functional

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ const blackBackgroundBtn = document.querySelector("#black-bkg");
 const whiteBackgroundBtn = document.querySelector("#white-bkg");
 const greyBackgroundBtn = document.querySelector("#grey-bkg");
 const sizeBtns = document.querySelectorAll(".size-btn");
+const blackSketchBtn = document.querySelector("#black-sketch");
+const whiteSketchBtn = document.querySelector("#white-sketch");
+const pastelSketchBtn = document.querySelector("#pastel-sketch");
+const neonSketchBtn = document.querySelector("#neon-sketch");
 
 //create grid
 function addDiv(n) {
@@ -59,7 +63,12 @@ sizeBtns.forEach((btn) => {
     addDiv(e.target.id);
 
     //when background is black, grid remains white when size is changed
-    if (grid.style.backgroundColor === "black") changeGridWhite();
+    if (grid.style.backgroundColor === "black") {
+      changeGridWhite();
+      whiteSketch();
+    } else {
+      blackSketch();
+    }
   });
 });
 
@@ -81,14 +90,79 @@ greyBackgroundBtn.addEventListener("click", () => {
 });
 
 //CHANGE SKETCH COLOR
-//read instructions
-//add black-sketch class (black background color)
-//apply black-sketch class on hover
-//make hover permanent
-//erase button to remove black-sketch class
-//add white-sketch class
-//write function to change color
-//add event listeners to black and white buttons
-//figure out how to get pastel and neon sketches
+
+//functions to sketch
+function blackSketch() {
+  const gridSquares = document.querySelectorAll(".grid div div");
+  gridSquares.forEach((div) => {
+    div.addEventListener("mouseover", () => {
+      div.classList.add("black-sketch");
+      div.classList.remove("white-sketch");
+      div.classList.remove("neon-sketch");
+      div.classList.remove("pastel-sketch");
+    });
+  });
+}
+
+function whiteSketch() {
+  const gridSquares = document.querySelectorAll(".grid div div");
+  gridSquares.forEach((div) => {
+    div.addEventListener("mouseover", () => {
+      div.classList.add("white-sketch");
+      div.classList.remove("neon-sketch");
+      div.classList.remove("black-sketch");
+      div.classList.remove("pastel-sketch");
+    });
+  });
+}
+
+function pastelSketch() {
+  const gridSquares = document.querySelectorAll(".grid div div");
+  gridSquares.forEach((div) => {
+    div.addEventListener("mouseover", () => {
+      div.classList.add("pastel-sketch");
+      div.classList.remove("white-sketch");
+      div.classList.remove("black-sketch");
+      div.classList.remove("neon-sketch");
+    });
+  });
+}
+
+function neonSketch() {
+  const gridSquares = document.querySelectorAll(".grid div div");
+  gridSquares.forEach((div) => {
+    div.addEventListener("mouseover", () => {
+      div.classList.add("neon-sketch");
+      div.classList.remove("white-sketch");
+      div.classList.remove("black-sketch");
+      div.classList.remove("pastel-sketch");
+    });
+  });
+}
+
+//buttons to change sketch color
+whiteSketchBtn.addEventListener("click", () => {
+  whiteSketch();
+});
+
+blackSketchBtn.addEventListener("click", () => {
+  blackSketch();
+});
+
+pastelSketchBtn.addEventListener("click", () => {
+  pastelSketch();
+});
+
+neonSketchBtn.addEventListener("click", () => {
+  neonSketch();
+});
+
+blackSketch();
 
 //ERASE SKETCH
+
+//add conditions to keep sketch colors when bkg color changes
+
+//make erase button functional
+
+//make neon and pastel buttons functional
